8b0058912a32d2c73c029bf88fe2707da04ab901
Vaibhav <vaibhavg1201@gmail.com>
2022-06-28 12:21:25 +0530

    commit

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -12,24 +12,31 @@ function ProjectCards(props) {
         <Card.Text style={{ textAlign: "justify" }}>
           {props.description}
         </Card.Text>
-        <span class="badge badge-primary font-weight-lighter ml-1">HTML</span> 
+        {/* <span class="badge badge-primary font-weight-lighter ml-1">HTML</span> 
         <span class="badge badge-secondary font-weight-lighter ml-1 ">CSS</span> 
         <span class="badge badge-success font-weight-lighter ml-1">JavaScript</span> 
         <span class="badge badge-danger font-weight-lighter ml-1">Node.JS</span> 
-        <span class="badge badge-info font-weight-lighter ml-1">Express</span> 
+        <span class="badge badge-info font-weight-lighter ml-1">Express</span>  */}
 
-        <Button variant="primary " href={props.link} target="_blank" className="ml-2 mt-4">
-          
+        <Button
+          variant="primary "
+          href={props.link}
+          target="_blank"
+          className="ml-2 mt-4"
+        >
           <BiLinkExternal /> &nbsp;
           {props.isBlog ? "View Blog" : "Code"}
         </Button>
 
-        <Button variant="primary " href={props.link1} target="_blank" className="ml-4 mt-4">
-          
+        <Button
+          variant="primary "
+          href={props.link1}
+          target="_blank"
+          className="ml-4 mt-4"
+        >
           <BiLinkExternal /> &nbsp;
           {props.isBlog ? "View Blog" : "View Project"}
-        </Button> 
-       
+        </Button>
       </Card.Body>
     </Card>
   );
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,6 +4,9 @@ import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import leaf from "../../Assets/Projects/leaf.png";
 import chatify from "../../Assets/Projects/chatify.png";
+import amazonClone from "../../Assets/amazonClone.png";
+import foodDelivery from "../../Assets/foodDelivery.png";
+import ecom from "../../Assets/ecom.png";
 
 function Projects() {
   return (
@@ -19,6 +22,17 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
+              imgPath={ecom}
+              isBlog={false}
+              title="E-com clothing store"
+              description="A simple e-commerce clothing application created using ReactJs with simple but effective UI & UX."
+              link="https://github.com/vaibhavjs/react-ecom-store.git"
+              link1="https://gregarious-sprite-e48322.netlify.app/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
               imgPath={chatify}
               isBlog={false}
               title="Myntra Clone"
@@ -30,11 +44,33 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
+              imgPath={amazonClone}
+              isBlog={false}
+              title="Amazon Clone"
+              description="This clone of popular e-commerce website - amazon.com is created using ReactJS & includes all the basic features of an e-commerce website"
+              link="https://github.com/vaibhavjs/clone-amazzon.git"
+              link1="https://clone-amazzon.vercel.app/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={foodDelivery}
+              isBlog={false}
+              title="Food Delivery App"
+              description="Developed using Redux + ReactJs, this food delivery application emulates the functionalities of popular indian food delivery applications."
+              link="https://github.com/vaibhavjs/redux-food-delivery-app.git"
+              link1="https://redux-food-delivery-app.vercel.app/home"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
               imgPath={leaf}
               isBlog={false}
               title="Drawing Application"
               description="Focusing on Vanilla Javascript + P5.js library, this drawing application replicates the the features of a classic drawing application - MS Paint."
-              link="https://github.com/vaibhavjs/DrawingApplication.git"
+              link="https://github.com/vaibhavjs/DrawingApp.git"
               link1="https://focused-morse-d9cbf0.netlify.app/"
             />
           </Col>
